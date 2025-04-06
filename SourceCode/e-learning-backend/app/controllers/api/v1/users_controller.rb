843e8cfc6d6037b2f2f03623e510efbecbc864e4
utnautiub <utnautiub@gmail.com>
2025-04-06 09:45:03 +0700

    init src, docker

---
+++ SourceCode/e-learning-backend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :set_user, only: [:show, :update, :destroy]
+
+      def index
+        users = User.all
+        render json: users, status: :ok
+      end
+
+      def show
+        render json: @user, status: :ok
+      end
+
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @user.update(user_params)
+          render json: @user, status: :ok
+        else
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @user.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_user
+        @user = User.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "User not found" }, status: :not_found
+      end
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :role)
+      end
+    end
+  end
+end
