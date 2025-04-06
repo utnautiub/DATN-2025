d7119bfcb8bf81ae69d1aef0dd3615cd48cfcaec
utnautiub <utnautiub@gmail.com>
2025-04-06 13:08:12 +0700

    add shadcn/vue to nuxt, config database local

--- SourceCode/e-learning-frontend/nuxt.config.ts
+++ SourceCode/e-learning-frontend/nuxt.config.ts
@@ -1,8 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   devServer: {
     port: 4000
+  },
+
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+  ],
+  colorMode: {
+    classSuffix: ''
+  },
+  shadcn: {
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
   },
 })
